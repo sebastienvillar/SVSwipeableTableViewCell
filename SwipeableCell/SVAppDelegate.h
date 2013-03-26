8c66f660864a51b79f376673801fe156b4495343
Sebastien Villar <sebastien.villar@gmail.com>
2013-03-26 13:01:01 +0200

    Initial Commit

---
+++ SwipeableCell/SVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SVAppDelegate.h
+//  SwipeableCell
+//
+//  Created by Sébastien Villar on 26/03/13.
+//  Copyright (c) 2013 Sébastien Villar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SwipeableCell/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SwipeableCell
+//
+//  Created by Sébastien Villar on 26/03/13.
+//  Copyright (c) 2013 Sébastien Villar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SVAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([SVAppDelegate class]));
+	}
+}
