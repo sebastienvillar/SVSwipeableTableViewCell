17b09781c71562fa571a9ef8133c72a3a43ba681
Sebastien Villar <sebastien.villar@gmail.com>
2013-03-26 13:11:54 +0200

    Add files - basic structure

---
+++ SwipeableCell/SVCellBackground.m
@@ -0,0 +1,31 @@
+//
+//  SVCellBackground.m
+//  SwipeableCell
+//
+//  Created by Sébastien Villar on 26/03/13.
+//  Copyright (c) 2013 Sébastien Villar. All rights reserved.
+//
+
+#import "SVCellBackground.h"
+
+@implementation SVCellBackground
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ SwipeableCell/SVSwipeableTableViewCell.m
@@ -0,0 +1,29 @@
+//
+//  SVSwipeableTableViewCell.m
+//  SwipeableCell
+//
+//  Created by Sébastien Villar on 26/03/13.
+//  Copyright (c) 2013 Sébastien Villar. All rights reserved.
+//
+
+#import "SVSwipeableTableViewCell.h"
+
+@implementation SVSwipeableTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
